9e3e402b6afe4a85c50ff6b9d6183589babc73a3
Pate1337 <paavo.hemmo@hotmail.com>
2017-11-14 17:45:40 +0200

    Tehtävä 1 tehty

---
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -0,0 +1,33 @@
+package ohtu;
+
+public class Submission {
+    private int week;
+    private int hours;
+    private int[] exercises;
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+    
+    public int getHours() {
+        return hours;
+    }
+    
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    @Override
+    public String toString() {
+        String tehtävät = "";
+        for (int tehtävä: exercises) {
+            tehtävät = tehtävät + tehtävä + " ";
+        }
+        return "viikko "+week + ": tehtyjä tehtäviä yhteensä: " + exercises.length + ", aikaa kului " + hours + " tuntia, tehdyt tehtävät: " + tehtävät;
+    }
+    
+}
